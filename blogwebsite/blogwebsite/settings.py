ada3d53ff4f0b167d8eb0da6e3dfe69d2da6af23
CharanCodeCraft <charangowak911@gmail.com>
2025-03-10 01:51:29 +0530

    deploy-static-problem

--- blogwebsite/blogwebsite/settings.py
+++ blogwebsite/blogwebsite/settings.py
@@ -122,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 LOGIN_REDIRECT_URL='blog-home'
 LOGIN_URL='login'
